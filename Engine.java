f7f3cbab5d3503a8a47fa477d832d25813d648a5
Vinothkumar22 <chandranvinoth22@gmail.com>
2019-05-07 21:52:34 +0530

    Engine class design.

---
+++ Engine.java
@@ -0,0 +1,35 @@
+package com.vanhack.hackerrank.work_hours;
+
+public class Engine extends Object {
+	private String type;
+	
+	public Engine(String type) {
+		this.type = type;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	@Override
+	public boolean equals(Object engine) {
+		if (engine == null) 			 return false;
+		if (!(engine instanceof Engine)) return false;
+		if (this == engine) 			 return true;
+		return this.type.equals(((Engine)engine).type);
+	}
+	
+	@Override
+	public int hashCode() {
+		return type.hashCode();
+	}
+	
+	@Override
+	public String toString() {
+		return type;
+	}
+}
